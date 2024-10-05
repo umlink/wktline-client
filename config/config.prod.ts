d4f3bf1157af81889573e590d21e8cf92dea1aaa
umlink <link_0403@163.com>
2024-10-05 22:39:26 +0800

    feat: for resourse

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -2,7 +2,15 @@ import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
   hash: true,
+  codeSplitting: {jsStrategy: 'depPerChunk'},
   chainWebpack(config) {
     config.output.chunkFilename('[contenthash:16].js');
+    config.plugin('compression-webpack-plugin').use(require('compression-webpack-plugin'), [
+      {
+        test: /.(js|css|html)$/i, // 匹配
+        threshold: 10240, // 超过10k的文件压缩
+        deleteOriginalAssets: false, // 不删除源文件
+      },
+    ])
   },
 });
